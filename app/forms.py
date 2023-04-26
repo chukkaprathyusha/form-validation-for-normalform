bb90700e4f48d213b58294f5a77ff36056db4c3b
chukkaprathyusha <chukkaprathyu@gmail.com>
2023-04-26 19:13:01 +0530

    first commit

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.core import validators
 
 def check_for_a(value):
     if value[0]=='a':
@@ -10,6 +11,22 @@ def Check_for_len(value):
         raise forms.ValidationError('len is less')
 
 class StudentForm(forms.Form):
-    name=forms.CharField(max_length=100,validators=[check_for_a])   
+    name=forms.CharField(max_length=100,validators=[check_for_a,validators.MaxLengthValidator(5)])   
     age=forms.IntegerField()
-    Email=forms.EmailField(validators=[check_for_a]) 
+    Email=forms.EmailField(validators=[check_for_a])
+    re_enter_email=forms.EmailField()
+    botcatcher=forms.CharField(max_length=100,widget=forms.HiddenInput,required=False)
+    mobile=forms.CharField(max_length=10,min_length=10,validators=[validators.RegexValidator('[6-9]\d{9}')])
+    
+
+
+    def Clean(self):
+        e=self.cleaned_data['email']
+        r=self.cleaned_data['re_enter_email']
+        if e!=r:
+            raise forms.ValidationError('Data not matched')
+            
+    def Clean_botcatcher(self):
+        bot=self.cleaned_data['botcatcher']
+        if len(bot)>0:
+            raise forms.ValidationError('bot')        
